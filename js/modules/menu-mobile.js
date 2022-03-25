296691996e1ec7e4530375a386286c670adaefdd
Matheus Borges <mattborgesdev@gmail.com>
2022-03-25 20:03:39 -0300

    outsideclick finalizado botao menu funcionando

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -1,19 +1,21 @@
-import outsideClick from "./outsideclick.js";
-
-outsideClick();
+import outsideClick from './outsideClick.js';
 
 export default function initMenuMobile() {
-
-    console.log('initMenuMobile funcionando!!');
-
-}
-
-const menuButton = document.querySelector('[data-menu="button"]');
-const menuList = document.querySelector('[data-menu="list"]');
-
-function openMenu(event) {
+  const menuButton = document.querySelector('[data-menu="button"]');
+  const menuList = document.querySelector('[data-menu="list"]');
+  const eventos = ['click', 'touchstart'];
+  
+  if(menuButton) {
+  function openMenu(event) {
     menuList.classList.add('active');
     menuButton.classList.add('active');
+    outsideClick(menuList, eventos, () => {
+    console.log('teste')
+  
+      menuList.classList.remove('active');
+      menuButton.classList.remove('active');
+    })
+  }
+  eventos.forEach(evento => menuButton.addEventListener(evento, openMenu));
+  }
 }
-
-menuButton.addEventListener('click', openMenu);
--- js/modules/outsideclick.js
+++ js/modules/outsideclick.js
@@ -1,5 +1,20 @@
-export default function outsideClick() {
-
-    console.log('outsideClick funcionando!!');
-
-}
+export default function outsideClick(element, events, callback) {
+    const html = document.documentElement;
+    const outside = 'data-outside';
+  
+    if(!element.hasAttribute(outside)) {
+      events.forEach(userEvent => {
+        setTimeout(() => html.addEventListener(userEvent, handleOutsideClick));
+      });
+      element.setAttribute(outside, '');
+    }
+    function handleOutsideClick(event) {
+      if(!element.contains(event.target)) {
+        element.removeAttribute(outside);
+        events.forEach(userEvent => {
+          html.removeEventListener(userEvent, handleOutsideClick);
+        })
+        callback();
+      }
+    }
+  }
