2349fc51cd8ef6b833fb320b779f93b4895691de
Matheus Borges <mattborgesdev@gmail.com>
2022-03-25 21:54:08 -0300

    removido bug do touchstart no menu-mobile

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -3,15 +3,13 @@ import outsideClick from './outsideclick.js';
 export default function initMenuMobile() {
   const menuButton = document.querySelector('[data-menu="button"]');
   const menuList = document.querySelector('[data-menu="list"]');
-  const eventos = ['click', 'touchstart'];
+  const eventos = ['click'];
   
   if(menuButton) {
   function openMenu(event) {
     menuList.classList.add('active');
     menuButton.classList.add('active');
     outsideClick(menuList, eventos, () => {
-    console.log('teste')
-  
       menuList.classList.remove('active');
       menuButton.classList.remove('active');
     })
